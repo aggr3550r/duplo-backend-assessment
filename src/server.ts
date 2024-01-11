e2b5ed57dc1785700aff1f1e218c35cc382150e5
Victor Uche <ucheviquetor@gmail.com>
2024-01-11 20:20:00 +0100

    fix: repairing server

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import Fastify from 'fastify';
 import 'reflect-metadata';
+import http from 'http';
 
 import postRoutes from './routes/post.routes';
 import userRoutes from './routes/user.routes';
+import { configService } from './config/config.service';
 
 const app = Fastify({ logger: true });
 
@@ -16,10 +18,13 @@ app.get('/api/v1/health', async (request, reply) => {
 app.register(postRoutes);
 app.register(userRoutes);
 
+const PORT = configService.getPort() || 8000;
+
 const start = async () => {
   try {
-    await app.listen(8000);
-    console.log(`Server is running on http://localhost:8000`);
+    app.listen(PORT, () => {
+      console.info(`HTTP Server started on port ${PORT}`);
+    });
   } catch (err) {
     console.error('Error starting server:', err);
     process.exit(1);
